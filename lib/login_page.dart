ba6216bd5d9c4a5f95143de8260555842539cd6c
shreyask12 <shreyashkotian@gmail.com>
2020-02-13 00:06:04 +0530

    LoginpageUi done

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -8,7 +9,12 @@ class LoginPageUI extends StatefulWidget {
 
 class _LoginPageUIState extends State<LoginPageUI> {
 
-  ScreenUtil screenutil = ScreenUtil();
+  // ScreenUtil screenutil;
+  @override
+  void initState() {
+    // screenutil = ScreenUtil();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,78 +27,164 @@ class _LoginPageUIState extends State<LoginPageUI> {
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           child: Stack(
-            alignment: Alignment.topCenter,
+            alignment: Alignment.topLeft,
           children: <Widget>[
             Container(
-              height: 400,
-              width: 360,
+              height: ScreenUtil().setHeight(400),
+              width: ScreenUtil().setWidth(360),
               decoration: BoxDecoration(
                 color: Color(0xff042c38),
                 image: DecorationImage(
                   image: AssetImage(
-                    'images/Mask Group 1.png',
+                    'assets/images/Mask Group 1.png',
                   ),
                   // fit: BoxFit.fill,
                 ),
               ),
             ),
             Positioned(
-              bottom: 10.0,
+              bottom: ScreenUtil().setWidth(0.0),
               child: 
-            Container(
-              width: 360,
-              height: 360,
-              decoration: BoxDecoration(
-                color: Color(0xffffffff),
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.only(
-                  topLeft:Radius.circular(30.0),
-                   topRight: Radius.circular(30.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom:ScreenUtil().setWidth(20.0),left:ScreenUtil().setWidth(20.0)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // Divider(color: Colors.white,thickness: 10.0,height: 10.0,),
+                      Text('Welcome',
+                      style: TextStyle(color:Colors.white,
+                        fontSize: ScreenUtil().setSp(20.0),
+                        letterSpacing: ScreenUtil().setWidth(3.0),
+                        fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top:ScreenUtil().setWidth(10.0),bottom:ScreenUtil().setWidth(10.0)),
+                        child: Text('to Room Control',style: TextStyle(color:Colors.white,
+                          fontSize: ScreenUtil().setSp(20.0),
+                          letterSpacing: ScreenUtil().setWidth(3.0),
+                          fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: EdgeInsets.only(left:30.0,right:30.0,top:50.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Container(
+                  width: ScreenUtil().setWidth(360),
+                  height: ScreenUtil().setWidth(360),
+                  decoration: BoxDecoration(
+                    color: Color(0xffffffff),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.only(
+                      topLeft:Radius.circular(ScreenUtil().setWidth(30.0)),
+                       topRight: Radius.circular(ScreenUtil().setWidth(30.0)),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left:ScreenUtil().setWidth(30.0),right:ScreenUtil().setWidth(30.0),top:ScreenUtil().setWidth(20.0)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: <Widget>[
-                      Icon(Icons.group),
-                        SizedBox(
-                          width: 250,
-                          child: TextField(
-                            decoration: InputDecoration(
-                            hintText: 'Username',
-                              hintStyle: TextStyle(
-                                color: Colors.black45,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                          Icon(Icons.group),
+                            Padding(
+                              padding: EdgeInsets.only(left:ScreenUtil().setWidth(32.0)),
+                              child: SizedBox(
+                                width: ScreenUtil().setWidth(230),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                  hintText: 'Username',
+                                    hintStyle: TextStyle(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            Icon(Icons.lock),
+                            Padding(
+                              padding: EdgeInsets.only(left:ScreenUtil().setWidth(32.0)),
+                              child: SizedBox(
+                                width: ScreenUtil().setWidth(230),
+                                child: TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                  hintText: 'Password',
+                                    hintStyle: TextStyle(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            
+                          ],
+                        ),
+                        Container(
+                          height: ScreenUtil().setHeight(60.0),
+                          width: ScreenUtil().setWidth(300),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Color(0xff00a79b),
+                            borderRadius: BorderRadius.all(Radius.circular(ScreenUtil().setWidth(5.0))),
                           ),
+                            child: Material(
+                              color: Colors.transparent,
+                            child: InkWell(
+                              splashColor: Colors.grey,
+                              child: Center(child: Text('SIGN IN',style: TextStyle(
+                                color:Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: ScreenUtil().setSp(24.0),
+                                letterSpacing: ScreenUtil().setWidth(3.0),
+                                  ),
+                                ),
+                          ),
+                            ),
+                            ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text('Dont have an account?',
+                            style: TextStyle(color:Color(0xffa6a6a6),
+                                fontSize: ScreenUtil().setSp(18.0),
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: (){},
+                              child: Padding(
+                                padding: EdgeInsets.only(left:ScreenUtil().setWidth(10.0)),
+                                child: Text('SIGN UP',style: TextStyle(color:Color(0xff00a79b),
+                                    fontSize: ScreenUtil().setSp(20.0),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Icon(Icons.lock),
-                        SizedBox(
-                          width: 250,
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                            hintText: 'Password',
-                              hintStyle: TextStyle(
-                                color: Colors.black45,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
             ),
           ],
